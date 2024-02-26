5b611953cc2e6c7485890c6dc8a8e5f9192f19d4
Bruno Bieth <biethb@gmail.com>
2024-02-26 20:49:09 +0100

    add a user attribute mapper (#67)

---
+++ src/main/java/at/klausbetz/provider/AppleUserAttributeMapper.java
@@ -0,0 +1,19 @@
+package at.klausbetz.provider;
+
+import org.keycloak.broker.oidc.mappers.UserAttributeMapper;
+
+public class AppleUserAttributeMapper extends UserAttributeMapper {
+
+    private static final String[] cp = new String[] { AppleIdentityProviderFactory.PROVIDER_ID };
+
+    @Override
+    public String[] getCompatibleProviders() {
+        return cp;
+    }
+
+
+    @Override
+    public String getId() {
+            return "apple-user-attribute-mapper";
+        }
+}
